7dd030e829d7f8b153e4bdabfe7f3c5f7bfa55e7
MauroChumpitazPolino <mauro.chumpitaz@utec.edu.pe>
2020-04-16 11:00:11 -0500

    finished_0
    Se terminó las funciones. Se probó con un cpp personal. Es la primera presentación, podría haber errores si se aplica la simulación del profe.

--- structures/stack.h
+++ structures/stack.h
@@ -3,25 +3,68 @@
 
 using namespace std;
 
-#define MAX 1000 
+#define MAX 1000
 
 // TODO: Implement all methods
 template <typename T>
 class stack {
 	T* data;
-	int top;
+	int itop;
 	int capacity;
 
 public:
-	stack(int size = MAX);
-	~stack();   		
+	stack(int size = MAX){
+		data = new T[size];
+		itop = -1;
+	}
+	~stack(){
+		delete data;
+	}   		
 
-	void push(T);
-	int pop();
-	int top();
+	void push(T value){
+		if (itop == (capacity - 1)){
+			cout << "Overflow instruction" << endl;
+		}
+		else{
+			itop++;
+			data[itop] = value;
+		}
+	}
 
-	int size();
-	bool empty();
+	int pop(){
+		if(itop < 0){
+			cout << "Underflow instruction" << endl;
+			return 0;
+		}
+		else{
+			itop = itop - 1;
+			return data[itop+1];
+		}
+	}
+
+	int top(){
+		if(itop < 0){
+			cout << "No hay top, stack vacío" << endl;
+			return 0;
+		}
+		else{
+			return data[itop];
+		}	
+	}
+
+	int size(){
+		return (itop+1);
+	}
+
+	bool empty(){
+		if(itop < 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
 };
 
 #endif
