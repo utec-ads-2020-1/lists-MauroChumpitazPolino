db5bda69d7eee87126624cb432949e1f960b3b61
MauroChumpitazPolino <mauro.chumpitaz@utec.edu.pe>
2020-04-18 22:32:19 -0500

    Finished_01
    Se agregó el funcionamiento dinámico a los contenedores, el 'size' predeterminado se cambió a 10 y se optimizó respuestas para el usuario en la función pop()

--- structures/queue.h
+++ structures/queue.h
@@ -1,9 +1,10 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <cstring>
 using namespace std;
 
-#define MAX 1000 
+#define MAX 10
 
 // TODO: Implement all methods
 template <typename T>
@@ -13,16 +14,97 @@ class queue {
 	int capacity;
 
 public:
-	stack(int size = MAX);
-	~stack();   		
+	queue(int size = MAX){
+		data = new T[size];
+		top = -1;
+		capacity = size;
+	}
+	~queue(){
+		delete[] data;
+	}	
 
-	void push(T);
-	int pop();
-	int front();
-    int back();
+	// Resize the Queue to enable push() function
+	void resize(){
+		size_t newSize = capacity * 2;
+		T* newData = new T[newSize];
 
-	int size();
-	bool empty();
+		memcpy(newData, data, capacity * sizeof(data[0]) );
+		capacity = newSize;
+		delete [] data; 
+		data = newData;
+	}
+
+	// Shift all values one position to right
+	void pushShift(){
+		for(int i = top; i >= 0; i--){
+			data[i+1] = data[i];
+		}
+	}
+
+	// Push one value into the Queue
+	void push(T newValue){
+		if (top >= (capacity - 1) ){
+			resize();
+		}
+		pushShift();
+		data[0] = newValue;
+		top++;
+	}
+
+	// Pop one value from Queue or send messages if needed
+	void pop(){
+		if (top < 0){
+			cout << "Underflow instruction" << endl;
+		}
+		else if (top == 0){
+			cout << "Queue is empty now" << endl;
+			top = -1;
+		}
+		else{
+			top = top - 1;
+		}
+	}
+
+	// Returns the front value or null if Queue is empty
+	T front(){
+		if (top < 0)
+		{
+			cout <<"Queue is empty, front is null" << endl;
+			return 0;
+		}
+		else{
+			return data[top];
+		}
+		
+	}
+
+	// Returns the back value or null if Queue is empty
+    T back(){
+		if (top < 0)
+		{
+			cout <<"Queue is empty, back is null" << endl;
+			return 0;
+		}
+		else{
+			return data[0];
+		}
+	}
+
+	// Returns the Queue size, zero if it is empty
+	int size(){
+		return (top+1);
+	}
+
+	// Return true if Queue is empty
+	bool empty(){
+		if(top < 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
 };
 
 #endif
--- structures/stack.h
+++ structures/stack.h
@@ -9,7 +9,6 @@ using namespace std;
 template <typename T>
 class stack {
 	T* data;
-	int itop;
 	int top;
 	int capacity;
 
@@ -20,49 +19,60 @@ public:
 		capacity = size;
 	}
 	~stack(){
-		delete data;
+		delete []data;
 	}   		
 
-	void push(T newValue){
-		// If stack is full, show 'overflow' message.
-		if (top >= (capacity - 1)){
-			cout << "Overflow instruction" << endl;
-		}
-		// Else, top goes to next position and add the new value.
-		else{
-			top++;
-			data[top] = newValue;
-		}
+	// Resize the Stack to enable push() function
+	void resize(){
+		size_t newSize = capacity * 2;
+		T* newData = new T[newSize];
+
+		memcpy(newData, data, capacity * sizeof(data[0]) );
+		capacity = newSize;
+		delete[] data; 
+		data = newData;
 	}
 
+	//Push one value into the Stack
+	void push(T newValue){
+		if (top >= (capacity - 1)){
+			resize();
+		}
+		top++;
+		data[top] = newValue;
+	}
+
+	// Pop one value from Stack or send messages if needed
 	void pop(){
-		// If stack is empty, show 'underflow' message.
-		if(top < 0){
+		if (top < 0){
 			cout << "Underflow instruction" << endl;
 		}
-		// Else, top goes to previous position.
+		else if (top == 0){
+			cout << "Stack is empty now" << endl;
+			top = -1;
+		}
 		else{
 			top = top - 1;
 		}
 	}
 
+	// Returns the peak value if Stack is not empty, else return a null
 	T peak(){
-		// If stack is empty, show 'no peak element' message.
 		if(top < 0){
-			cout << "No peak element, empty stack" << endl;
+			cout << "Null peak, stack is empty" << endl;
 			return 0;
 		}
-		// Else, return the peak element.
 		else{
 			return data[top];
 		}	
 	}
 
+	// Returns the Queue size, zero if it is empty
 	int size(){
-		// Because first stack element starts in 'top = 0', the stack size is (top + 1).
 		return (top+1);
 	}
 
+	// Return true if Queue is empty
 	bool empty(){
 		if(top < 0){
 			return true;
